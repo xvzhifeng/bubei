c59031c80e9c7468dc18c91bfc0edf9f33308c7c
徐志锋 <xvzhifeng@126.com>
2022-01-31 03:30:53 +0800

    init front End Code

---
+++ frontEnd/bubei/src/pages/index/index.vue
@@ -0,0 +1,235 @@
+<template >
+  <view class="content" :style="{ height: appHeight + 'rpx' }">
+    <image class="image-bg" :src="backgroundUrl" />
+    <view class="userIconView">
+      <cover-image
+        src="/static/logo.png"
+        class="userIcon"
+        @click="gotoUserPage()"
+      ></cover-image>
+    </view>
+    <view class="perWord">
+      <text> Hyacinth </text>
+    </view>
+
+    <view class="buttonView" >
+      <view classs="subButtonView1">
+        <button :disabled="false" :loading="false" @click="study()" plain="true" class="button">
+          <view class="subButtonView1_1">
+            <text class="BurronText">Learn</text>
+            <text class="BurronTextCount">{{ learnCount }}</text>
+          </view>
+          
+        </button>
+      </view>
+
+      <view classs="subButtonView2" >
+        <button
+          :disabled="false"
+          :loading="false"
+          @click="review()"
+          plain="true"
+          class="button"
+        >
+          <view class="subButtonView1_1">
+            <text class="BurronText">Review</text>
+            <text class="BurronTextCount">{{ reviewCount }}</text>
+          </view>
+        </button>
+      </view>
+    </view>
+    <!-- bottom icon -->
+    <view class="buttonIcon">
+      <uni-icons type="map" size="30" @click="home()" />
+      <uni-icons type="star" size="30" @click="collecte()" />
+      <uni-icons type="headphones" size="30" @click="listen()" />
+      <uni-icons type="paperplane" size="30" @click="statistics()" />
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello",
+      learnCount: 1000,
+      reviewCount: 20,
+      appHeight: 0,
+      backgroundUrl: "/static/temp/index.jpg",
+    };
+  },
+  onLoad() {
+    uni.getSystemInfo({
+      success: (res) => {
+        console.log("手机可用高度:" + res.windowHeight * 2 + "rpx");
+        this.appHeight = res.windowHeight * 2;
+      },
+    });
+  },
+
+  methods: {
+    gotoUserPage() {
+      uni.showToast({
+        title: "跳转用户页面",
+        icon: "success",
+        mask: true,
+      });
+      uni.navigateTo({ url: "/pages/index/user" ,
+	  success:()=>{
+		  console.log("success goto user page");
+	  }});
+    },
+    study() {
+      uni.showToast({
+        title: "Learn",
+        icon: "success",
+        mask: true,
+      });
+    },
+    review() {
+      uni.showToast({
+        title: "review",
+        icon: "success",
+        mask: true,
+      });
+    },
+    home() {
+      //todo
+      uni.showToast({
+        title: "home",
+        icon: "success",
+        mask: true,
+      });
+    },
+    collecte() {
+      //todo
+      uni.showToast({
+        title: "collecte",
+        icon: "success",
+        mask: true,
+      });
+    },
+    listen() {
+      //todo
+      uni.showToast({
+        title: "listen",
+        icon: "success",
+        mask: true,
+      });
+    },
+    statistics() {
+      //todo
+      uni.showToast({
+        title: "statistics",
+        icon: "success",
+        mask: true,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+.image-bg {
+  position: absolute;
+  z-index: -1;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+}
+.content {
+  /* background: #63639c; */
+  position: relative;
+  z-index: 0;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.userIconView {
+  /* display: flex;
+	align-items: left;
+	justify-content: left; */
+  position: absolute;
+  left: 0;
+  top: 0;
+  align-self: start;
+}
+
+.userIcon {
+  width: 120rpx;
+  height: 120rpx;
+  border-radius: 100px;
+}
+.buttonView {
+  display: flex;
+  justify-content: space-around;
+  width: 750rpx;
+  flex-basis: 20upx;
+ 
+  margin: 0 auto;
+}
+.button{
+  padding: 0rpx;
+}
+.subButtonView1 {
+  width: 280rpx;
+  height: 120rpx;
+  border-radius: 50%;
+ display: inline-block;
+  /* opacity: 0.3; */
+  padding: 0px;
+}
+
+.subButtonView1_1 {
+  width: 250rpx;
+  display: flex;
+  flex-direction: column;
+  background: rgba(58, 57, 57,0.4);
+
+}
+
+.subButtonView2 {
+  border-radius: 100px;
+width: 280rpx;
+  /* opacity: 0.3;
+  filter: blur(20px); */
+  display: inline-block;
+}
+.perWord {
+  font-size: 60rpx;
+  display: flex;
+  flex-basis: 500upx;
+  
+
+}
+.BurronText {
+  font-size: 28rpx;
+  color: rgb(58, 57, 57);
+  width: 120rpx;
+  display: block;
+  
+
+}
+.BurronTextCount {
+  font-size: 28rpx;
+  color: rgb(233, 197, 37);
+  width: 120rpx;
+  line-height: 110%;
+}
+
+.buttonIcon {
+  display: flex;
+  justify-content: space-around;
+  width: 750rpx;
+  position: absolute;
+  bottom: 75rpx;
+  color: rgb(255, 251, 246);
+}
+</style>
---
+++ frontEnd/bubei/src/pages/login/login.vue
@@ -0,0 +1,216 @@
+<template>
+  <view class="content" :style="{ height: appHeight + 'rpx' }">
+    <view class="title1">
+      <text class="font1">不</text>
+    </view>
+    <view class="title2">
+      <text class="font2">背</text>
+    </view>
+    <view class="title3">
+      <text class="font3">真实语境学单词</text>
+    </view>
+
+    <!-- 底部布局 -->
+    <view class="bottom">
+      <view class="emailButton">
+        <button
+          class="email"
+          :disabled="false"
+          :loading="false"
+          hover-class="button-hover"
+          @click="register()"
+        >
+          邮箱一键登录
+        </button>
+      </view>
+
+      <view class="subButton">
+        <button class="subButton1" :disabled="true" @click="todo()">
+          <uni-icons type="chatboxes-filled" size="25"></uni-icons>
+        </button>
+        <button class="subButton1" :disabled="true" @click="todo()">
+          <uni-icons type="email" size="25"></uni-icons>
+        </button>
+        <button class="subButton1" :disabled="true" @click="todo()">
+          <uni-icons type="more" size="25"></uni-icons>
+        </button>
+      </view>
+
+      <view class="option">
+        <checkbox-group @change="checkboxChange">
+          <label class="checkboxs">
+            <checkbox
+              :value="isRead"
+              :checked="false"
+              color="rgb(250, 166, 96)"
+              style="transform: scale(0.7)"
+            />&nbsp;&nbsp;
+            我已成功阅读并同意《隐私协议》《服务条款》和《儿童信息保护》
+          </label>
+        </checkbox-group>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello",
+      isRead: "isRead",
+      pitchOn: false,
+      appHeight:0,
+    };
+  },
+  onLoad() {
+    uni.getSystemInfo({
+      success: (res) => {
+        console.log("手机可用高度:" + res.windowHeight * 2 + "rpx");
+        this.appHeight = res.windowHeight * 2;
+      },
+    });
+  },
+  methods: {
+    register() {
+      if (!this.pitchOn) {
+        alert("请先阅读相关条例");
+        return;
+      } else {
+        uni.navigateTo({ url: '/pages/login/email' })({
+            url:"/pages/login/email",
+            successs: () => {
+                console.log("跳转到邮箱输入页面成功");
+            },
+            fail: () => {
+                console.log("跳转邮箱输入页面失败");
+            }
+        })
+        console.log("注册");
+      }
+    },
+    checkboxChange(e) {
+        console.log(...e.detail.value)
+        let [temp] = [...e.detail.value] 
+        this.pitchOn = temp === 'isRead';
+    },
+  },
+};
+</script>
+
+<style>
+.content {
+  background-color: rgb(58, 57, 57);
+  background-size: auto;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* justify-items: center; */
+  /* color: rgb(59, 58, 58); */
+}
+.title1 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  background: rgb(58, 57, 57);
+  /* padding: 20px; */
+  border: 2px solid rgb(250, 166, 96);
+  width: 124rpx;
+  height: 124rpx;
+  margin: 6rpx;
+}
+.title2 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  background: rgb(250, 166, 96);
+  /* padding: 20px; */
+  width: 124rpx;
+  height: 124rpx;
+  margin: 6rpx;
+}
+
+.title3 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  /* background: rgb(250, 166, 96); */
+  /* padding: 20px; */
+  width: 400px;
+  height: 40px;
+  margin: 10rpx;
+}
+
+.font1 {
+  color: rgb(250, 166, 96);
+  font-size: 80rpx;
+}
+
+.font2 {
+  color: #313131;
+  font-size: 90rpx;
+}
+
+.font3 {
+  color: #ffffff;
+  font-size: uni-font-size-base;
+}
+
+.font4 {
+  color: #ffffff;
+  font-size: 20px;
+  margin: 10px;
+}
+
+.bottom {
+  flex-direction: column;
+  display: flex;
+  justify-content: end;
+  align-items: center;
+  height: 400px;
+}
+
+.subButton {
+  width: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* align-items: center; */
+  /* align-self: end; */
+}
+
+.subButton1 {
+  /* align-self: flex-end; */
+  height: 50px;
+  width: 50px;
+  border-radius: 800px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.email {
+  width: 200px;
+  height: 50px;
+  margin: 10px;
+  background-color: rgb(242, 245, 53);
+}
+
+.option {
+  margin: 20px;
+  width: 300px;
+  height: 50px;
+  font-size: 10px;
+}
+
+.checkboxs {
+  height: 40px;
+  color: rgb(242, 245, 53);
+}
+</style>
